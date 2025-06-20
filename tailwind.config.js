2fb0b20c1aaa15fa6b4b1e69a47bf9d3d123ef16
Cesar Ocampo <cesar.ocampo.9457@gmail.com>
2025-06-20 23:42:36 +1000

    frontend

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/**
+ * @type {import('tailwindcss').Config}
+ */
+
+export const content = [
+  "./index.html",
+
+
+  "./src/**/*.{js,ts,jsx,tsx}",
+];
+export const purge = [];
+export const darkMode = false;
+export const theme = {
+  extend: {},
+};
+export const variants = {
+  extend: {},
+};
+export const plugins = [];
